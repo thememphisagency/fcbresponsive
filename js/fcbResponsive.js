7a65787122fb7a1803e4e73b33e2e3b025968fd0
Tim Parkinson <tim@thememphisagency.com>
2013-01-14 09:55:47 +1030

    Adding javascript to reorder aside element based on media query

--- js/fcbResponsive.js
+++ js/fcbResponsive.js
@@ -1,11 +1,35 @@
 $(document).ready(function() {
-  $('body').addClass('js');
-  var $menu = $('#menu'),
+	$('body').addClass('js');
+  	var $menu = $('#menu'),
     $menulink = $('.toggleMenu');
-  
-$menulink.click(function() {
-	console.log('what');
-  $menulink.toggleClass('active');
-  $menu.toggleClass('active');
-  return false;
-});});
+	$menulink.click(function() {
+		
+		$menulink.toggleClass('active');
+		$menu.toggleClass('active');
+		return false;
+	});
+
+  	$.fn.appendAround = function(){
+	    return this.each(function(){
+		    console.log('test');
+		      
+		    var $self = $( this ),
+		        att = "data-set",
+		        $set = $( "["+ att +"='"+ $self.closest( "["+ att +"]" ).attr( att ) + "']" );
+		     
+		    function appendToVisibleContainer(){
+		    	console.log($set);
+		        if( $self.is( ":hidden" ) ){
+		          $self.appendTo( $set.filter( ":visible:eq(0)" ) );
+		        }
+		    }
+		      
+		    appendToVisibleContainer();
+		      
+		    $(window).resize( appendToVisibleContainer );
+		      
+	    });
+  	};
+  	$(".sponsors").appendAround();
+});
+
