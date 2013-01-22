839d08021d0155d8b18aa13f67eb93baffa0b1b7
Jon Low <jon@thememphisagency.com>
2013-01-22 11:35:23 +1030

    Remote appendAround code from document.ready and add some comments

--- js/fcbResponsive.js
+++ js/fcbResponsive.js
@@ -9,25 +9,35 @@ $(document).ready(function() {
 		return false;
 	});
 
-  	$.fn.appendAround = function(){
-	    return this.each(function(){
-
-		    var $self = $( this ),
-		        att = "data-set",
-		        $set = $( "["+ att +"='"+ $self.closest( "["+ att +"]" ).attr( att ) + "']" );
-		     
-		    function appendToVisibleContainer(){
-		        if( $self.is( ":hidden" ) ){
-		          $self.appendTo( $set.filter( ":visible:eq(0)" ) );
-		        }
-		    }
-		      
-		    appendToVisibleContainer();
-		      
-		    $(window).resize( appendToVisibleContainer );
-		      
-	    });
-  	};
   	$(".sponsors").appendAround();
 });
 
+
+/*! appendAround markup pattern. [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL 
+	https://github.com/filamentgroup/AppendAround
+how-to:
+	1. Insert potential element containers throughout the DOM
+	2. give each container a data-set attribute with a value that matches all other containers' values
+	3. Place your appendAround content in one of the potential containers
+	4. Call appendAround() on that element when the DOM is ready
+*/
+
+$.fn.appendAround = function(){
+	return this.each(function(){
+
+	    var $self = $( this ),
+	        att = "data-set",
+	        $set = $( "["+ att +"='"+ $self.closest( "["+ att +"]" ).attr( att ) + "']" );
+	     
+	    function appendToVisibleContainer(){
+	        if( $self.is( ":hidden" ) ){
+	          $self.appendTo( $set.filter( ":visible:eq(0)" ) );
+	        }
+	    }
+	      
+	    appendToVisibleContainer();
+	      
+	    $(window).resize( appendToVisibleContainer );
+	      
+	});
+};
