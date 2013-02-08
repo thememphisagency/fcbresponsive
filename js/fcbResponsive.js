147f36946f79669500f8313c46995a22b35c6da3
matthewattanasio <matthew@thememphisagency.com>
2013-02-08 11:07:44 +1030

    added js from master

---
+++ js/fcbResponsive.js
@@ -0,0 +1,67 @@
+/* 
+	Use the comment-based directives to compress and minify all js files into one 
+*/
+
+// @depends lib/jquery-1.8.3.min.js
+// @depends lib/respond.min.js
+// @depends lib/enquire.min.js
+// @depends lib/appendAround.js
+// @depends lib/fastclick.js
+
+
+$(document).ready(function() {	
+	/* 
+		All functionality that requires responsive     
+		Should be included here
+	*/	
+	enquire.register("screen and (min-width:320px) and (max-width:991px)", {
+		match: function() {
+			$('.toggleBtn.search').on("click", function(){
+				$('.header-search-form').toggleClass('active');
+			});
+		},
+
+		unmatch: function() {
+			$('.toggleBtn.search').off("click");
+		}
+	})
+	.register("screen and (min-width:992px)",{
+		match: function() {
+			//Turn the searchToggle button into a search submit
+		},
+
+		unmatch: function() {
+		}
+	})
+	.listen();
+
+	$(".sponsors").appendAround();
+
+  	var $menu = $('#menu'),
+    $menulink = $('.toggleBtn.menu');
+	$menulink.click(function() {
+		
+		$menulink.toggleClass('active');
+		$menu.toggleClass('active');
+		return false;
+	});
+
+	// Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+	if (Modernizr.touch && !window.location.hash) {
+		$(window).load(function () {
+		  setTimeout(function () {
+		    // At load, if user hasn't scrolled more than 20px or so...
+					if( $(window).scrollTop() < 20 ) {
+		      window.scrollTo(0, 1);
+		    }
+		  }, 0);
+		});
+	}
+
+	// Instantiate FastClick
+		window.addEventListener('load', function() {
+	    new FastClick(document.body);
+	}, false);
+
+
+});
